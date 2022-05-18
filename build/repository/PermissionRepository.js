88b267e7ba950a30bf8a8db59249c043d8b9a718
Davi <davihgs20@gmail.com>
2022-05-18 12:14:10 -0300

    Creating new standard build

--- build/repository/PermissionRepository.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PermissionRepository = void 0;
-const dataSource_1 = require("../dataSource");
-const Permission_1 = require("../model/Permission");
-exports.PermissionRepository = dataSource_1.AppDataSource.getRepository(Permission_1.Permission).extend({});
--- build/services/PermissionService.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PermissionService = void 0;
-class PermissionService {
-}
-exports.PermissionService = PermissionService;
